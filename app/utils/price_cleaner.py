c83be144de8bf5d8085b3288aeaedfc868642344
Restyled.io <commits@restyled.io>
2024-05-03 13:40:13 +0000

    Restyled by black

--- app/utils/price_cleaner.py
+++ app/utils/price_cleaner.py
@@ -10,15 +10,13 @@ def clean_price(price_str):
     :return: Den bereinigten Preis als String im Dezimalformat, oder None bei einem Fehler.
     """
     try:
-        normalized_str = re.sub(r'[^\d\.]', '', price_str)
-        match = re.search(r'\d+(\.\d+)?', normalized_str)
+        normalized_str = re.sub(r"[^\d\.]", "", price_str)
+        match = re.search(r"\d+(\.\d+)?", normalized_str)
         if match:
             return f"{float(match.group(0)):.2f}"
         else:
-            logging.warning(
-                f"Keine Zahl im Preis-String {price_str} gefunden.")
+            logging.warning(f"Keine Zahl im Preis-String {price_str} gefunden.")
             return None
     except Exception as e:
-        logging.error(
-            f"Fehler beim Bereinigen des Preis-Strings {price_str}: {e}")
+        logging.error(f"Fehler beim Bereinigen des Preis-Strings {price_str}: {e}")
         return None
